7141637a67a9fb66b37c0c0475a5746e18e3341e
shauntagates6911-ops <shaunta.gates6911@gmail.com>
2026-08-26 21:56:44 -0400

    Add domain restriction to reject.js

---
+++ reject.js
@@ -0,0 +1,8 @@
+if (window.location.hostname !== "bhutan-coding.github.io") {
+    document.body.innerHTML = `
+        <div style="text-align:center;padding:40px;font-size:32px;">
+            🚫 Bhutan Coding API<br>
+            <small>Domain not allowed</small>
+        </div>
+    `;
+}
